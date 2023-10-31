46eca9639bb24206ec155685cad6ee0b1886d1cc
HAIYUN NING <haiyunning@faustud-010-021-043-175.pool.uni-erlangen.de>
2023-10-31 16:23:33 +0100

    ipynb

--- opti_nash.py
+++ opti_nash.py
@@ -6,22 +6,29 @@ import time
 
 class Game:
     """
-    pay_off: player A's payoff
+    payoff: player A's payoff
     scores: based on opponent strategy, and trials, the final scores received by player A
     player_descion: player's strategy probability distribution
 
     """
-    pay_off = np.array(
+    payoff_s = np.array(
+        [[0, -1, 1],
+         [1, 0, -1],
+         [-1, 1, 0]]
+    )
+
+    payoff_a = np.array(
         [[-2, 1, 2],
          [2, -1, 0],
          [1, 0, -2]]
     )
+    payoff_b = -1 * payoff_a.transpose()
     scores = 0
-    player_descion = [0, 0, 0, 1, 1, 1, 1, 1, 2]
+    player_a_strategy_profile = [0] * 7 + [1] * 5 + [2] * 6
+    player_b_strategy_profile = [0] * 3 + [1] * 5 + [2] * 1
 
     def __init__(self, num_of_trials: int, num_of_threads: int):
         """
-
         :param num_of_trials: the total number of gaming trials
         :param num_of_threads: number of parallelizing thread, as default
         equal to the number of cores, it will reduce performance when threads large than cores,
@@ -42,9 +49,9 @@ class Game:
         local_score = 0
         while COUNT:
             COUNT -= 1
-            playera_descion = np.random.choice(self.player_descion)
-            playerb_descion = np.random.choice(self.player_descion)
-            local_score += self.pay_off[playera_descion, playerb_descion]
+            playera_descion = np.random.choice(self.player_a_strategy_profile)
+            playerb_descion = np.random.choice(self.player_b_strategy_profile)
+            local_score += self.payoff_a[playera_descion, playerb_descion]
         return local_score
 
     def run_with_thread(self):
@@ -68,18 +75,18 @@ class Game:
               f'total trials = {self.number_of_trials}')
 
 
-def opti_strategy(pay_off_matrix, player_position: bool):
+def opti_strategy(payoff_matrix, player_position: bool):
     """
 
-    :param pay_off matrix: outcomes of a strategic interaction between two or more decision-makers
+    :param payoff matrix: outcomes of a strategic interaction between two or more decision-makers
     :param player_position:
     :return:
     """
-    # pay off matrix of player A
-    pay_off = pay_off_matrix
+    # payoff matrix of player A
+    payoff = payoff_matrix
 
-    # opponent pay off matrix
-    pay_off_op = -1 * pay_off.transpose()
+    # opponent payoff matrix
+    payoff_op = -1 * payoff.transpose()
 
     # z coefficient
     z_coe = np.array([-1, -1, -1]).reshape((3, 1))
@@ -89,9 +96,9 @@ def opti_strategy(pay_off_matrix, player_position: bool):
 
     # Coefficients of the inequality constraints (Ax >= b)
     if not player_position:  # if player position 0
-        Ine_M = -1 * np.append(pay_off, z_coe, axis=1)
+        Ine_M = -1 * np.append(payoff, z_coe, axis=1)
     else:  # if player position 1
-        Ine_M = -1 * np.append(pay_off_op, z_coe, axis=1)
+        Ine_M = -1 * np.append(payoff_op, z_coe, axis=1)
 
     Ine_b = np.array([0, 0, 0])
 
@@ -99,7 +106,7 @@ def opti_strategy(pay_off_matrix, player_position: bool):
     E_M = np.array([[1, 1, 1, 0]])
     E_b = np.array([1])
 
-    # Bounds for variables (1>= xi >= 0)
+    # Bounds for variables (0 <= xi <= 1)
     x1 = (0, 1)
     x2 = (0, 1)
     x3 = (0, 1)
@@ -111,24 +118,26 @@ def opti_strategy(pay_off_matrix, player_position: bool):
     # Print the results
     print("Status:", result.message)
     print("Optimal Values (x1, x2, x3):", result.x[:-1])
-    print("expected scores can be achieved", result.fun[-1])
+    print("expected scores can be achieved", result.x[-1])
 
 
 if __name__ == '__main__':
-    pay_off = np.array([[-2, 1, 2],
-                        [2, -1, 0],
-                        [1, 0, -2]]
-                       )
-    player = 0  # player position 0 or 1
-    opti_strategy(pay_off, bool(player))
+    payoff = np.array([[-2, 1, 2],
+                       [2, -1, 0],
+                       [1, 0, -2]]
+                      )
+    player = 1  # player position 0 or 1
+    opti_strategy(payoff, bool(player))
 
     try:
         number_of_threads = multiprocessing.cpu_count()
     except Exception as e:
         print(f'Error detecting CPU count: {e}')
         number_of_threads = 1
+
     Start_Game = Game(1000000, number_of_threads)
     start = time.perf_counter()
     Start_Game.run_with_thread()
-    print(f'performance of {number_of_threads} threads is {time.perf_counter() - start}')
+    print(
+        f'performance of {number_of_threads} threads is {(time.perf_counter() - start) / number_of_threads} per thread')
     Start_Game.result()
