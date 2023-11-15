1fa690c4c1d9f8598a67f73141db28042f00d26b
HAIYUN NING <haiyunning@gmx.com>
2023-11-15 15:49:58 +0100

    code organize

--- code/lecture_images.py
+++ code/lecture_images.py
@@ -3,10 +3,14 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-path = '../../lecture_images/'
-items = os.listdir(path)[1:]
 
 def clear_image(image, image_shape):
+    """
+    add more contrast to the image
+    :param image: the input image
+    :param image_shape: the input image shape
+    :return: modified image based on the image shape
+    """
     assert isinstance(image, np.ndarray), 'image type should be a numpy array'
     assert len(image_shape) == 2, 'image_shape is not correct'
     image_shape.reverse()
@@ -20,6 +24,11 @@ def clear_image(image, image_shape):
     return modified_image
 
 def show_image(number_of_images, *args):
+    """
+    show all images in one figure
+    :param number_of_images: number of images want to display
+    :param args: images which are wanted to be display
+    """
     assert number_of_images > 0, 'number of images is invalid'
 
     fig, ax = plt.subplots(1, number_of_images)
@@ -36,21 +45,31 @@ def show_image(number_of_images, *args):
         plt.close()
 
 
+if __name__ == '__main__':
+    path = '../../lecture_images/'
+    items = os.listdir(path)[1:]
+    assert items, 'items in the target directory are not accessible '
+    for item in items:
+        try:
+            image = cv2.imread(path + item)
+        except Exception:
+            raise 'not able to read the image'
+        image = np.array(image)
+        image_shape = [int(i/3) for i in list(np.shape(image)[:2])][::-1] #change the shape to the third of original length and width
+        resized_image = cv2.resize(image, image_shape)
 
-for item in items:
-    try:
-        image = cv2.imread(path + item)
-    except Exception:
-        raise 'not able to read the image'
-    image = np.array(image)
-    image_shape = [int(i/3) for i in list(np.shape(image)[:2])][::-1] #change the shape to the third of original length and width
-    resized_image = cv2.resize(image, image_shape)
-    # src = cv2.cvtColor(resized_image, cv2.COLOR_RGB2GRAY)
-    # ret, resized_image = cv2.threshold(src, 125, 255, cv2.THRESH_BINARY)
-    # kernel = np.ones((5, 5), np.uint8)
-    # morphology = cv2.morphologyEx(src, cv2.MORPH_CLOSE, kernel, iterations=2)
-    # cv2.imwrite(item, resized_image)
-    
+        src = cv2.cvtColor(resized_image, cv2.COLOR_RGB2GRAY)
+        show_image(1, src)
+        # fig, ax = plt.subplots(1, 1)
+        # ax.imshow(resized_image)
+        # plt.show()
+
+        # src = cv2.cvtColor(resized_image, cv2.COLOR_RGB2GRAY)
+        # ret, resized_image = cv2.threshold(src, 125, 255, cv2.THRESH_BINARY)
+        # kernel = np.ones((5, 5), np.uint8)
+        # morphology = cv2.morphologyEx(src, cv2.MORPH_CLOSE, kernel, iterations=2)
+        # cv2.imwrite(item, resized_image)
+
 
 
 
